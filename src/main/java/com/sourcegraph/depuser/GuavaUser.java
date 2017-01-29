251f7f6857c701deb2ddb17d3de6719e3f1e630a
Beyang Liu <beyang@sourcegraph.com>
2017-01-29 15:58:49 -0800

    guava usages

---
+++ src/main/java/com/sourcegraph/depuser/GuavaUser.java
@@ -0,0 +1,32 @@
+package com.sourcegraph.depuser;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GuavaUser {
+    public void callStrings() {
+        Strings.isNullOrEmpty("hi");
+    }
+
+    public void callJoin() {
+        List<String> parts = Arrays.asList("a", "b", "c", null);
+        String joined = Joiner.on(", ").skipNulls().join(parts);
+    }
+
+    public void callIterables() {
+        List<String> names = Lists.newArrayList("a", "b", "c");
+        Iterable<Integer> nameSizes1 = Iterables.transform(names,
+                new Function<String, Integer>() {
+                    public Integer apply(String s) {
+                        return s.length();
+                    }
+                }
+        );
+    }
+}
