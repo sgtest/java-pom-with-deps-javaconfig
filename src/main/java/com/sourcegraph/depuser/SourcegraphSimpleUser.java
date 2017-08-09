3c87c67986ed483264e67c1548fd184f58644dec
Beyang Liu <beyang@sourcegraph.com>
2017-08-08 23:34:33 -0700

    add java-simple (at https://bintray.com/sourcegraph/simple) dependency

---
+++ src/main/java/com/sourcegraph/depuser/SourcegraphSimpleUser.java
@@ -0,0 +1,12 @@
+package com.sourcegraph.depuser;
+
+import com.sourcegraph.simple.Map;
+import com.sourcegraph.simple.MyInterface;
+
+/**
+ * Created by beyang on 8/8/17.
+ */
+public class SourcegraphSimpleUser {
+    private Map map;
+    private MyInterface myInterface;
+}
